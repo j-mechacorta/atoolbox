264e6e00a94b181079171884c29a3a7d50d9d149
Adrian Alanis <adrian.alanis@me.com>
2020-11-06 21:17:18 +0000

    new functions: toJson and add_numeric_fields - toJson: convert dataframe to json array instead of json records - add_numeric_fields: per row add all numeric columns

--- tests/test_multiple_df_joins.py
+++ tests/test_multiple_df_joins.py
@@ -40,3 +40,6 @@ def test_left_join_dataframes():
     print(expected_cols)
     print(joined_df.columns)
     assert joined_df.columns == expected_cols
+
+def test_inner_join_dataframes():
+    pass
